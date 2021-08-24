b3afb8594adf983ba526f23f4a0377932df9b978
Ruth Moyano <ruth.moyano@codeimmersives.com>
2021-08-24 14:11:12 -0500

    issues fixed with answers

--- src/components/Questionaire.js
+++ src/components/Questionaire.js
@@ -1,40 +1,45 @@
 import React from 'react';
 
-const Button = ({ answer, className }) => (
-    <button 
-        className={`bg-white p-4 text-purple-800 
-        font-semibold rounded shadow ${className}`}>
-        {answer}
-    </button>
-);
-
 const Questionaire = ({
+    showAnswers,
     handleAnswer, 
     data: { question, correct_answer, 
     incorrect_answers },
 }) => {
     const shuffledAnswers = [correct_answer, ...
-    incorrect_answers].sort(() => Math.random() - 0.5);
+    incorrect_answers].sort(
+        () => Math.random() - 0.5
+        );
+
     return (
     <div>
         <div className="bg-white text-purple-800 p-10
     rounded shadow-md">
     <h2 
         className="text-2xl" 
-        dangerouslySetInnerHTML={{ __html: question }}>
-    </h2>
+        dangerouslySetInnerHTML={{ __html: question }}
+    />
     </div>
     <div className="grid grid-cols-2 gap-6 mt-6">
-        {shuffledAnswers.map((answer) => (
+        {shuffledAnswers.map((answer) => {
+            const bgColor = showAnswers
+            ? answer === correct_answer
+                ? 'bg-green-500'
+                : 'bg-red-500'
+            : 'bg-white';
+
+            //const textColor = showAnswers ?
+            //'text-white' : 'text-purple-800'
+            return (    
             <button
-                className='bg-white p-4
-                text-purple-800 font-semibold
-                rounded shadow'
+                className={`${bgColor} ${textColor}
+                p-4 font-semibold
+                rounded shadow`}
                 onclick={() => handleAnswer
                 (answer)}
-                dangerouslySetInnerHTML= {{ __html: answer}
+                dangerouslySetInnerHTML= {{ __html: answer}}
                 />
-                ))}
+                );
             </div>
         </div>
     );
