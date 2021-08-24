015cee4bb5bcffa99aa60f95a19383b40820acb8
Ruth Moyano <ruth.moyano@codeimmersives.com>
2021-08-24 10:58:06 -0500

    changes made

--- src/App.js
+++ src/App.js
@@ -1,29 +1,48 @@
-import { QuestionAnswerSharp } from '@material-ui/icons';
 import React, { useState, useEffect } from 'react';
 
 import { Questionaire } from './components';
 
 const API_URL = 
-"https://opentdb.com/api.php?amount=15&category=10&difficulty=easy";
+"https://opentdb.com/api.php?amount=15&category=10&difficulty=easy&type=multiple";
 
 function App () {
   const [questions, setQuestions] = useState([]);
+  const [currentIndex, setCurrentIndex] = 
+  useState(0);
+  const [score, setScore] = useState(0);
+  const [gameEnded, setGameEnded] = useState(false);
 
   useEffect(() => {
     fetch(API_URL)
       .then((res) => res.json())
       .then((data) => {
         setQuestions(data.results);
+        setCurrentQuestions(data.results[0])
       });
   }, []);
 
   const handleAnswer = (answer) => {
   //check for answer 
+  const newIndex = currentIndex + 1
+  setCurrentIndex(newIndex);
+
+  if (answer === questions[currentIndex].currentIndex) {
+    setScore(score + 1);
+  }
+
+  if(newIndex>= questions.length) {
+    setGameEnded(true);
+  }
   };
 
-  return questions.length > 0 ? (
+  return gameEnded ? (
+    <h1 className='text-3xl text-white font-bold'>Your final score: {score}</h1>
+  ) : questions.length > 0 ? (
     <div className="container">
-      <Questionaire data={questions[0]} handleAnswer={handleAnswer} />
+      {currentQuestion && (
+      <Questionaire data=
+      {questions[currentIndex]} handleAnswer={handleAnswer} />
+      )}
     </div>
   ) : (
     <h2 className="text-2xl text-white font-bold">Loading...</h2>
