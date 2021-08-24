191919bc2ebb0d2451af8370a529ba92636ddf17
Ruth Moyano <ruth.moyano@codeimmersives.com>
2021-08-24 11:29:53 -0500

    issues fixed

--- src/components/Questionaire.js
+++ src/components/Questionaire.js
@@ -2,13 +2,18 @@ import React from 'react';
 
 const Button = ({ answer, className }) => (
     <button 
-    className={'bg-white p-4 text-purple-800 font-semibold rounded shadow ${className}'}>
-    {answer}
+        className={`bg-white p-4 text-purple-800 
+        font-semibold rounded shadow ${className}`}>
+        {answer}
     </button>
 );
 
-const Questionaire = ({handleAnswer, data: {question, correct_answer, incorrect_answers }}) => {
-    const shuffledAnswer = [correct_answer, ...
+const Questionaire = ({
+    handleAnswer, 
+    data: { question, correct_answer, 
+    incorrect_answers },
+}) => {
+    const shuffledAnswers = [correct_answer, ...
     incorrect_answers].sort(() => Math.random() - 0.5);
     return (
     <div>
@@ -20,18 +25,20 @@ const Questionaire = ({handleAnswer, data: {question, correct_answer, incorrect_
     </h2>
     </div>
     <div className="grid grid-cols-2 gap-6 mt-6">
-        <Button className={correct_answer === shuffledAnswer[0] ? 'bg-purple-300' : ''}
-        onCLick={() => handleAnswer(shuffledAnswer[0])} answer={shuffledAnswer[0]} />
-        <Button className={correct_answer === shuffledAnswer[1] ? 'bg-purple-300' : ''}
-        onCLick={() => handleAnswer(shuffledAnswer[1])} answer={shuffledAnswer[1]} />
-        <Button className={correct_answer === shuffledAnswer[2] ? 'bg-purple-300' : ''}
-        onCLick={() => handleAnswer(shuffledAnswer[2])} answer={shuffledAnswer[2]} />
-        <Button className={correct_answer === shuffledAnswer[3] ? 'bg-purple-300' : ''}
-        onCLick={() => handleAnswer(shuffledAnswer[3])} answer={shuffledAnswer[3]} />
-    </div>
-    </div>
-    )
-}
+        {shuffledAnswers.map((answer) => (
+            <button
+                className='bg-white p-4
+                text-purple-800 font-semibold
+                rounded shadow'
+                onclick={() => handleAnswer
+                (answer)}
+                dangerouslySetInnerHTML= {{ __html: answer}
+                />
+                ))}
+            </div>
+        </div>
+    );
+};
 
 
 export default Questionaire;
