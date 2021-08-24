ef92f4b29af83670f47179928de1516069bf6adc
Ruth Moyano <ruth.moyano@codeimmersives.com>
2021-08-24 02:08:23 -0500

    changes made

--- src/components/Questionaire.js
+++ src/components/Questionaire.js
@@ -1,14 +1,15 @@
 import React from 'react';
 
-const Button = ({ answer }) => (
-    <button className="bg-white p-4 text-purple-800 font-semibold rounded shadow mb-4">
+const Button = ({ answer, className }) => (
+    <button 
+    className={'bg-white p-4 text-purple-800 font-semibold rounded shadow ${className}'}>
     {answer}
     </button>
 );
 
 const Questionaire = ({handleAnswer, data: {question, correct_answer, incorrect_answers }}) => {
     const shuffledAnswer = [correct_answer, ...
-    incorrect_answers];
+    incorrect_answers].sort(() => Math.random() - 0.5);
     return (
     <div>
         <div className="bg-white text-purple-800 p-10
@@ -19,10 +20,14 @@ const Questionaire = ({handleAnswer, data: {question, correct_answer, incorrect_
     </h2>
     </div>
     <div className="grid grid-cols-2 gap-6 mt-6">
-        <Button onCLick={() => handleAnswer(shuffledAnswer[0])} answer={shuffledAnswer[0]} />
-        <Button onCLick={() => handleAnswer(shuffledAnswer[1])} answer={shuffledAnswer[1]} />
-        <Button onCLick={() => handleAnswer(shuffledAnswer[2])} answer={shuffledAnswer[2]} />
-        <Button onCLick={() => handleAnswer(shuffledAnswer[3])} answer={shuffledAnswer[3]} />
+        <Button className={correct_answer === shuffledAnswer[0] ? 'bg-purple-300' : ''}
+        onCLick={() => handleAnswer(shuffledAnswer[0])} answer={shuffledAnswer[0]} />
+        <Button className={correct_answer === shuffledAnswer[1] ? 'bg-purple-300' : ''}
+        onCLick={() => handleAnswer(shuffledAnswer[1])} answer={shuffledAnswer[1]} />
+        <Button className={correct_answer === shuffledAnswer[2] ? 'bg-purple-300' : ''}
+        onCLick={() => handleAnswer(shuffledAnswer[2])} answer={shuffledAnswer[2]} />
+        <Button className={correct_answer === shuffledAnswer[3] ? 'bg-purple-300' : ''}
+        onCLick={() => handleAnswer(shuffledAnswer[3])} answer={shuffledAnswer[3]} />
     </div>
     </div>
     )
